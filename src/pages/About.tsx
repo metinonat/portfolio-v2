aa733d35b1198358d77a1f70016cb0431fa3f32f
metinonat <onatcukur@gmail.com>
2024-02-08 01:53:22 +0300

    refactor: assets moved to public

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ export default function About(): JSX.Element {
               <div className="-ml-12 -mt-20 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                 <img
                   className="w-[28rem] rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[35rem]"
-                  src="/src/assets/me.jpg"
+                  src={PROFILE.image}
                   alt=""
                 />
               </div>
