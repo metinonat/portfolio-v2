034da509934b11b10ad0902d9518606fcd3e099f
metinonat <onatcukur@gmail.com>
2024-02-19 04:32:09 +0300

    vite base update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-v2/",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
